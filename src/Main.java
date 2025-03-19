4eb3a0994bf8feabbf291af0f2d17face8648b65
fugu2000 <9662506@graniteschools.org>
2025-03-19 12:57:04 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,9 @@
+static private int gravity, wave, waveDifficulty;
+static private boolean start, gameRun, upgrade, death;
 
+static void startWave() {}
+static void draw() {}
+static void infoOverlay() {}
+static void upgradeScreen() {}
+static void deathScreen() {}
+static void runMusic() {}
