c73a3a51cb25bbd703dc6397447556348228477f
fugu2000 <9662506@graniteschools.org>
2025-03-24 11:50:47 -0600

    Create projectile.java

---
+++ src/projectile.java
@@ -0,0 +1,9 @@
+// Marcus Nishikawa
+private static int speed, damage, x, y;
+private static String name;
+private static boolean intersect;
+
+private static void move()
+  {}
+private statc int getDamage()
+  {}
