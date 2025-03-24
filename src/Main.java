34f3249291af44537e4a11bea3a61b57cb57e2a3
fugu2000 <9662506@graniteschools.org>
2025-03-24 12:24:11 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
-public class main
+// Marcus Nishikawa
+
+static private int gravity, wave, waveDifficulty;
+static private boolean start, gameRun, upgrade, death;
+PImage startImage;
+
+private static void startWave() {}
+void setup()
+{
+  startImage = loadImage("Sang.jpg");
+  size(1800, 600);
+  start = true;
+}
+void draw() 
+{
+  if (start)
   {
-  static private int gravity, wave, waveDifficulty;
-  static private boolean start, gameRun, upgrade, death;
-  
-  static void startWave() {}
-  static void draw() {}
-  static void infoOverlay() {}
-  static void upgradeScreen() {}
-  static void deathScreen() {}
-  static void runMusic() {}
+    startScreen();
   }
+}
+private void startScreen()
+{
+  image(startImage, 0, 0);
+}
+private static void infoOverlay() {}
+private static void upgradeScreen() {}
+private static void deathScreen() {}
+private static void runMusic() {}
