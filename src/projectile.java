7f717776a4551c11cb5e1d3e3cf55ba34fb75a88
fugu2000 <9662506@graniteschools.org>
2025-03-24 11:57:46 -0600

    Update projectile.java

--- src/projectile.java
+++ src/projectile.java
@@ -1,9 +1,14 @@
 // Marcus Nishikawa
-private static int speed, damage, x, y;
-private static String name;
-private static boolean intersect;
-
-private static void move()
-  {}
-private statc int getDamage()
-  {}
+public class projectile
+{
+  private int speed, damage, x, y;
+  private String name;
+  private boolean intersect;
+  
+  private void move()
+    {}
+  private int getDamage()
+    {
+      return damage;
+    }
+}
