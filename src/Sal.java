591e2fd192dd87b49327e365ad08c56e20807c54
fugu2000 <9662506@graniteschools.org>
2025-03-24 11:14:06 -0600

    Update Sal.java

--- src/Sal.java
+++ src/Sal.java
@@ -1,3 +1,7 @@
+private static float x, y, xSpeed, ySpeed;
+private static int width, height, blood, totalBlood;
+private static String weaponSelected, currentAction;
+private static boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
 void display(){
 }
 void move(){
