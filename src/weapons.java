1cbed72bd77a79ae2c9d088455f0f36abda60d23
fugu2000 <9662506@graniteschools.org>
2025-03-24 11:12:31 -0600

    Update weapons.java

--- src/weapons.java
+++ src/weapons.java
@@ -3,4 +3,5 @@ public abstract class weapons
       static private String name;
       static private int damage, cooldown, bloodCost;
       static private boolean available, onCooldown;
+      
   }
