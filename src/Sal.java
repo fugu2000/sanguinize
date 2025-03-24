61e0a4a963e1d986673034c716001dec1955e5e0
fugu2000 <9662506@graniteschools.org>
2025-03-24 11:51:29 -0600

    Update Sal.java

--- src/Sal.java
+++ src/Sal.java
@@ -1,14 +1,17 @@
-private static float x, y, xSpeed, ySpeed;
-private static int width, height, blood, totalBlood;
-private static String weaponSelected, currentAction;
-private static boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
-void display(){
-}
-void move(){
-}
-void dash(){
-}
-void jump(){
-}
-void bloodWield(){
+public class sal
+{
+  private static float x, y, xSpeed, ySpeed;
+  private static int width, height, blood, totalBlood;
+  private static String weaponSelected, currentAction;
+  private static boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
+  void display(){
+  }
+  void move(){
+  }
+  void dash(){
+  }
+  void jump(){
+  }
+  void bloodWield(){
+  }
 }
