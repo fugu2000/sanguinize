33d7e96c6d8eb79216b786f93ff1348ede2bbf6b
fugu2000 <9662506@graniteschools.org>
2025-03-28 12:33:34 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,116 @@
-public class Sal
+import gifAnimation.*;
+
+Sal s1;
+static public Gif sIdle;
+static public int gravity, wave, waveDifficulty, ground;
+static public float friction;
+static public char jumpKey, leftKey, rightKey;
+static private boolean start, gameRun, upgrade, death, jumpKeyHeld, leftKeyHeld, rightKeyHeld;
+PImage startText;
+PImage startBack;
+PImage mainBack;
+PFont dos;
+
+void setup()
 {
-  Gif s1;
-  private float x, y, xSpeed, ySpeed;
-  private int sWidth, sHeight, blood, totalBlood, jumpHeight;
-  private String weaponSelected, currentAction;
-  private boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
-  
-  // PImage sal;
-  Sal()
+  friction = .8;
+  jumpKey = 'w';
+  leftKey = 'a';
+  rightKey = 'd';
+  ground = height - 300;
+  s1 = new Sal();
+  sIdle = new Gif(this, "theguy3.gif");
+  startText = loadImage("startText.png");
+  startBack = loadImage("startBack.png");
+  mainBack = loadImage("Map.png");
+  size(1720, 900);
+  start = true;
+  wave = 1;
+  gravity = 5;
+  waveDifficulty = 10;
+  dos = createFont("PerfectDOSVGA437.ttf", 35);
+}
+void draw() 
+{
+  if (start)
   {
-    x = width / 2;
-    y = height / 2;
-    jumpHeight = 20;
-    sWidth = 100;
-    sHeight = 100;
-    blood = 0;
-    totalBlood = 0;
-    
+    startScreen();
   }
-  
-  void display()
+  else if (gameRun)
   {
-    imageMode(CENTER);
-    sIdle.play();
-    image(sIdle, x, y);
-    if (y >= ground)
-    {
-      inAir = false;
-      y = ground;
-      if(ySpeed > 0)
-      {
-        ySpeed = 0;
-      }
-    }
-    else
-    {
-      inAir = true;
-    }
-  }
-  void move()
-  {
-    x += xSpeed;
-    y += ySpeed;
-    if(inAir)
-    {
-      ySpeed += gravity;
-    }
-    xSpeed *= friction;
-    ySpeed *= friction;
-  }
-  void walk(int tempX)
-  {
-    xSpeed += tempX;
-  }
-  void dash(){
-  }
-  void jump()
-  {
-    if(!inAir)
-    {
-      ySpeed -= jumpHeight;
-    }
-  }
-  void bloodWield(){
+    controlScheme();
+    infoOverlay();
+    s1.display();
+    s1.move();
   }
 }
+void mousePressed()
+{
+  if (start)
+  {
+    gameRun = true;
+    start = false;
+  }
+}
+void keyPressed()
+{
+  if (key == jumpKey)
+  {
+    jumpKeyHeld = true;
+  }
+  if (key == leftKey)
+  {
+    leftKeyHeld = true;
+  }
+  if (key == rightKey)
+  {
+    rightKeyHeld = true;
+  }
+}
+void keyReleased()
+{
+  if (key == jumpKey)
+  {
+    jumpKeyHeld = false;
+  }
+  if (key == leftKey)
+  {
+    leftKeyHeld = false;
+  }
+  if (key == rightKey)
+  {
+    rightKeyHeld = false;
+  }
+}
+void controlScheme()
+{
+  if(jumpKeyHeld)
+  {
+    s1.jump();
+  }
+  if(leftKeyHeld)
+  {
+    s1.walk(-5);
+  }
+  if(rightKeyHeld)
+  {
+    s1.walk(5);
+  }
+}
+private static void startWave() {}
+private void startScreen()
+{
+  imageMode(CORNER);
+  image(startBack, 0, 0);
+  imageMode(CENTER);
+  image(startText, width / 2, height / 2);
+}
+
+private void infoOverlay()
+{
+  imageMode(CORNER);
+  image(mainBack, 0, 0);
+}
+private static void upgradeScreen() {}
+private static void deathScreen() {}
+private static void runMusic() {}
