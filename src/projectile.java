41ea89a4d4f7c21338d5623d543d770f038d5ec9
fugu2000 <9662506@graniteschools.org>
2025-03-28 12:04:38 -0600

    Update projectile.java

--- src/projectile.java
+++ src/projectile.java
@@ -1,12 +1,12 @@
-// Marcus Nishikawa
 public class projectile
 {
-  private int speed, damage, x, y;
+  private int direction, speed, damage, x, y;
   private String name;
   private boolean intersect;
   
   private void move()
-    {}
+    {
+    }
   private int getDamage()
     {
       return damage;
