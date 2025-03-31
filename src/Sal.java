e21f77c2c0a5a8e82dccda68b5d55ecde598d654
fugu2000 <9662506@graniteschools.org>
2025-03-31 11:56:46 -0600

    Update Sal.java

--- src/Sal.java
+++ src/Sal.java
@@ -1,29 +1,36 @@
 public class Sal
 {
   Gif s1;
-  private float x, y, xSpeed, ySpeed;
+  private float x, y, xSpeed, ySpeed, dashSpeed;
   private int sWidth, sHeight, blood, totalBlood, jumpHeight;
   private String weaponSelected, currentAction;
   private boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
-  
-  // PImage sal;
+  private String direction = "left";
   Sal()
   {
     x = width / 2;
     y = height / 2;
     jumpHeight = 20;
-    sWidth = 100;
+    sWidth = 18;
     sHeight = 100;
     blood = 0;
     totalBlood = 0;
-    
+    dashSpeed = 30;
   }
   
   void display()
   {
     imageMode(CENTER);
-    sIdle.play();
-    image(sIdle, x, y);
+    if(direction.equals("left"))
+    {
+      sIdleLeft.play();
+      image(sIdleLeft, x, y);
+    }
+    else
+    {
+      sIdleRight.play();
+      image(sIdleRight, x, y);
+    }
     if (y >= ground)
     {
       inAir = false;
@@ -48,12 +55,42 @@ public class Sal
     }
     xSpeed *= friction;
     ySpeed *= friction;
+    if(x < sWidth)
+    {
+      x = 0 + sWidth;
+    }
+    if(x > width - sWidth)
+    {
+      x = width - sWidth;
+    }
   }
   void walk(int tempX)
   {
     xSpeed += tempX;
+    if(tempX < 0)
+    {
+      direction = "left";
+    }
+    else
+    {
+      direction = "right";
+    }
   }
-  void dash(){
+  void dash()
+  {
+    if(dashTime.isFinished())
+    {
+      if(direction == "left")
+      {
+        xSpeed -= dashSpeed;
+        dashTime.start();
+      }
+      else
+      {
+        xSpeed += dashSpeed;
+        dashTime.start();
+      }
+    }
   }
   void jump()
   {
