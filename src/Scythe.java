b437b9e330c27d980f829a0767e417a075a1da32
Jerry(Yangshengbin) <415330@graniteschools.org>
2025-04-21 11:54:01 -0600

    Update Scythe.java

--- src/Scythe.java
+++ src/Scythe.java
@@ -1,68 +1,124 @@
-public class Scythe extends Weapons {
-  PImage scytheImg;
-  private float x, y;
-  private boolean isAttacking = false;
-  private boolean isReturning = false;
-  private int attackStartFrame = 0;
-  private int cooldownStartFrame = -200;
-  private float attackDuration = 60;
-  private float cooldownDuration = 120;
-  private float scytheAngle = 0;
-  private float startAngle = -PI / 4;
-  private float endAngle = PI / 4;
+public class Scythe extends Weapon {
+    private float currentAngle;
+    private float attackStartAngle;
+    private float attackEndAngle;
+    private boolean isSwingReturning;
+    private PImage weaponImage;
+    private int swingDirection;
+    private float swingSpeed;
+    private float hitboxRadius;
+    private ArrayList<PVector> hitPositions;
 
-  Scythe(float x, float y) {
-    name = "Scythe";
-    damage = 25;
-    cooldown = 2;
-    bloodCost = 50;
-    this.x = x;
-    this.y = y;
-    scytheImg = loadImage("SCYTHE.PNG");
-  }
-
-  public void attack() {
-    if (!isAttacking && frameCount - cooldownStartFrame >= cooldownDuration) {
-      isAttacking = true;
-      isReturning = false;
-      attackStartFrame = frameCount;
-      cooldownStartFrame = frameCount;
-      Player.health -= bloodCost;
-    }
-  }
-
-  public void update() {
-    if (isAttacking) {
-      int elapsed = frameCount - attackStartFrame;
-      float progress = (float)elapsed / attackDuration;
-      if (!isReturning) {
-        if (progress <= 1.0) {
-          scytheAngle = lerp(startAngle, endAngle, progress);
-        } else {
-          isReturning = true;
-          attackStartFrame = frameCount;
+    public Scythe(PApplet processingContext) {
+        super("Scythe", 35, 90, 30);
+        this.currentAngle = 0;
+        this.attackStartAngle = -PI/3;
+        this.attackEndAngle = PI/3;
+        this.isSwingReturning = false;
+        this.swingSpeed = 0.15f;
+        this.hitboxRadius = 100f;
+        this.swingDirection = 1;
+        this.hitPositions = new ArrayList<PVector>();
+        
+        try {
+            this.weaponImage = processingContext.loadImage("weapons/scythe.png");
+            this.weaponImage.resize(120, 120);
+        } catch (Exception e) {
+            System.err.println("Failed to load scythe image: " + e.getMessage());
         }
-      } else {
-        float returnProgress = (float)(frameCount - attackStartFrame) / attackDuration;
-        if (returnProgress <= 1.0) {
-          scytheAngle = lerp(endAngle, startAngle, returnProgress);
-        } else {
-          isAttacking = false;
-          scytheAngle = 0;
-        }
-      }
     }
-  }
 
-  public void display() {
-    if (scytheImg != null) {
-      pushMatrix();
-      translate(x, y);
-      rotate(scytheAngle);
-      imageMode(CENTER);
-      image(scytheImg, 0, 0, 120, 120);
-      popMatrix();
+    public void update() {
+        super.update();
+        
+        if (isAttacking) {
+            if (!isSwingReturning) {
+                currentAngle = lerp(attackStartAngle, attackEndAngle, attackProgress);
+                if (attackProgress >= 1.0f) {
+                    isSwingReturning = true;
+                    attackProgress = 0f;
+                }
+            } else {
+                currentAngle = lerp(attackEndAngle, attackStartAngle, attackProgress);
+                if (attackProgress >= 1.0f) {
+                    isAttacking = false;
+                    isSwingReturning = false;
+                    hitPositions.clear();
+                }
+            }
+            attackProgress += swingSpeed;
+            
+            updateHitboxPositions();
+        }
     }
-  }
+
+    private void updateHitboxPositions() {
+        hitPositions.clear();
+        float radius = 80f;
+        float x = cos(currentAngle) * radius;
+        float y = sin(currentAngle) * radius;
+        hitPositions.add(new PVector(x, y));
+    }
+
+    protected void onAttackStart(float x, float y, String direction) {
+        isAttacking = true;
+        isSwingReturning = false;
+        attackProgress = 0f;
+        
+        if (direction.equals("left")) {
+            attackStartAngle = -PI/2;
+            attackEndAngle = PI/2;
+            swingDirection = -1;
+        } else {
+            attackStartAngle = PI/2;
+            attackEndAngle = -PI/2;
+            swingDirection = 1;
+        }
+        currentAngle = attackStartAngle;
+    }
+
+    protected void onAirAttack(float x, float y) {
+    }
+
+    public void render(float x, float y, String direction) {
+        if (weaponImage != null) {
+            pushMatrix();
+            translate(x, y);
+            rotate(currentAngle);
+            imageMode(CENTER);
+            image(weaponImage, 0, 0);
+            popMatrix();
+        } else {
+            pushMatrix();
+            translate(x, y);
+            rotate(currentAngle);
+            fill(150, 75, 0);
+            rect(0, 0, 100, 20);
+            popMatrix();
+        }
+    }
+
+    public ArrayList<PVector> getHitboxPositions() {
+        return new ArrayList<PVector>(hitPositions);
+    }
+
+    public float getHitboxRadius() {
+        return hitboxRadius;
+    }
+
+    public boolean isInAttack() {
+        return isAttacking;
+    }
+
+    public boolean canInterrupt() {
+        return isSwingReturning && attackProgress > 0.5f;
+    }
+
+    public void cancelAttack() {
+        if (canInterrupt()) {
+            isAttacking = false;
+            isSwingReturning = false;
+            hitPositions.clear();
+        }
+    }
 }
-
