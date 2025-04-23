9f2412a4b8391bbe23e3c7efb0280b34c3a146c0
Jerry(Yangshengbin) <415330@graniteschools.org>
2025-04-23 11:43:07 -0600

    Update Weapons.java

--- src/Weapons.java
+++ src/Weapons.java
@@ -1,120 +1,24 @@
-import processing.core.*;
-import java.util.ArrayList;
-
 public abstract class Weapon {
-    protected PApplet parent;
-    protected String name;
-    protected int damage;
-    protected int cooldown;
-    protected int bloodCost;
-    protected int currentCooldown;
-    protected boolean isActive;
-    protected boolean isAttacking;
-    protected float attackProgress;
-    protected PImage weaponImage;
-    protected ArrayList<PVector> hitPoints;
-    protected float hitRadius;
-    protected String lastDirection;
+    public String name;
+    public int damage;
+    public int cooldown;
+    public int bloodCost;
+    public boolean available;
+    public boolean onCooldown;
 
-    public Weapon(PApplet parent, String name, int damage, int cooldown, int bloodCost) {
-        this.parent = parent;
+    public Weapon(String name, int damage, int cooldown, int bloodCost) {
         this.name = name;
         this.damage = damage;
         this.cooldown = cooldown;
         this.bloodCost = bloodCost;
-        this.currentCooldown = 0;
-        this.isActive = true;
-        this.isAttacking = false;
-        this.attackProgress = 0;
-        this.hitPoints = new ArrayList<PVector>();
-        this.hitRadius = 50;
-        this.lastDirection = "right";
+        this.available = true;
+        this.onCooldown = false;
     }
 
-    public void update() {
-        if (currentCooldown > 0) {
-            currentCooldown--;
-        }
-        
-        if (isAttacking) {
-            attackProgress += 0.05f;
-            updateAttack();
-            
-            if (attackProgress >= 1.0f) {
-                isAttacking = false;
-                attackProgress = 0;
-            }
-        }
+    public boolean canUse() {
+        return !onCooldown;
     }
 
-    protected abstract void updateAttack();
-
-    public void startAttack(float x, float y, String direction) {
-        if (currentCooldown <= 0 && !isAttacking) {
-            isAttacking = true;
-            attackProgress = 0;
-            currentCooldown = cooldown;
-            lastDirection = direction;
-            onAttackStart(x, y, direction);
-        }
-    }
-
-    protected abstract void onAttackStart(float x, float y, String direction);
-
-    public void render(float x, float y) {
-        if (weaponImage != null && isAttacking) {
-            parent.pushMatrix();
-            parent.translate(x, y);
-            if (lastDirection.equals("left")) {
-                parent.scale(-1, 1);
-            }
-            renderWeapon();
-            parent.popMatrix();
-        }
-    }
-
-    protected abstract void renderWeapon();
-
-    public ArrayList<PVector> getHitPoints() {
-        return new ArrayList<PVector>(hitPoints);
-    }
-
-    public float getHitRadius() {
-        return hitRadius;
-    }
-
-    public boolean isAttacking() {
-        return isAttacking;
-    }
-
-    public boolean canAttack() {
-        return currentCooldown <= 0 && !isAttacking;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-    public boolean hasMovementEffect() {
-        return false;
-    }
-
-    public PVector getMovementEffect() {
-        return new PVector(0, 0);
-    }
-
-    protected void loadImage(String imagePath) {
-        try {
-            weaponImage = parent.loadImage(imagePath);
-            if (weaponImage != null) {
-                weaponImage.resize(100, 100);
-            }
-        } catch (Exception e) {
-            System.err.println("Failed to load weapon image: " + e.getMessage());
-        }
-    }
+    public abstract void mainAttack(Sal s, Direction dir);
+    public abstract void bloodwield(Sal s, Direction dir);
 }
