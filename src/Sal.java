1f5178990fd61deecccdf1aead68c37bd3c85de2
fugu2000 <9662506@graniteschools.org>
2025-05-07 12:29:39 -0600

    Update Sal.java

--- src/Sal.java
+++ src/Sal.java
@@ -1,104 +1,151 @@
 public class Sal
 {
   Gif s1;
-  private float x, y, xSpeed, ySpeed, dashSpeed;
-  private int sWidth, sHeight, blood, totalBlood, jumpHeight;
+  private float xSpeed, ySpeed, dashSpeed;
+  public float x, y, totalBlood;
+  private int sWidth, sHeight, jumpHeight, flashes, tempFlashes, level;
+  private float bloodDrain;
+  public float blood, maxBlood;
   private String weaponSelected, currentAction;
-  private boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead;
-  private String direction = "left";
+  private boolean isDashing, inAir, isAttacking, isHit, iFrame, isDead, flashing;
+  public String direction = "left";
+  final private Timer dashFrames = new Timer(100);
+  final private Timer damageFrames = new Timer(100);
+  final private Timer flashFrames = new Timer(100);
   Sal()
   {
+    level = 1;
+    flashing = false;
+    tempFlashes = 0;
+    maxBlood = 200;
     x = width / 2;
     y = height / 2;
     jumpHeight = 20;
     sWidth = 18;
     sHeight = 100;
-    blood = 0;
+    blood = 190;
     totalBlood = 0;
     dashSpeed = 30;
+    bloodDrain = 0.1;
   }
-  
+
   void display()
   {
+    if (dashFrames.isFinished() && damageFrames.isFinished()) iFrame = false;
+    if (flashFrames.isFinished()) flashing = false;
     imageMode(CENTER);
-    if(direction.equals("left"))
+    if (mouseX < x)
     {
       sIdleLeft.play();
+      direction = "left";
+      if (flashing) tint(255, 0, 0);
       image(sIdleLeft, x, y);
-    }
-    else
+      noTint();
+    } else
     {
+      direction = "right";
       sIdleRight.play();
+      if (flashing) tint(255, 0, 0);
       image(sIdleRight, x, y);
+      noTint();
     }
     if (y >= ground)
     {
       inAir = false;
       y = ground;
-      if(ySpeed > 0)
+      if (ySpeed > 0)
       {
         ySpeed = 0;
       }
-    }
-    else
+    } else
     {
       inAir = true;
+    }
+    if (blood >= maxBlood)
+    {
+      upgrade = true;
+      level ++;
+      bloodDrain = level / 10;
+      maxBlood = 200 * level;
+      blood = maxBlood / 2;
+    }
+    if (gameRun) blood -= bloodDrain * timeScale;
+    if (blood < 0) {
+      death = true;
+      gameRun = false;
     }
   }
   void move()
   {
-    x += xSpeed;
-    y += ySpeed;
-    if(inAir)
+    x += xSpeed * timeScale;
+    y += ySpeed * (timeScale);
+    if (inAir)
     {
-      ySpeed += gravity;
+      ySpeed += gravity * timeScale;
     }
-    xSpeed *= friction;
-    ySpeed *= friction;
-    if(x < sWidth)
+    xSpeed *= pow(friction, timeScale);
+    ySpeed *= pow(friction, timeScale);
+    if (x < sWidth)
     {
       x = 0 + sWidth;
     }
-    if(x > width - sWidth)
+    if (x > width - sWidth)
     {
       x = width - sWidth;
     }
   }
   void walk(int tempX)
   {
-    xSpeed += tempX;
-    if(tempX < 0)
+    xSpeed += tempX * timeScale;
+    if (tempX < 0)
     {
       direction = "left";
-    }
-    else
+    } else
     {
       direction = "right";
     }
   }
   void dash()
   {
-    if(dashTime.isFinished())
+    if (dashTime.isFinished())
     {
-      if(direction == "left")
+      if (direction == "left")
       {
         xSpeed -= dashSpeed;
+        iFrame = true;
+        dashFrames.start();
         dashTime.start();
-      }
-      else
+      } else
       {
         xSpeed += dashSpeed;
+        iFrame = true;
+        dashFrames.start();
         dashTime.start();
       }
     }
   }
   void jump()
   {
-    if(!inAir)
+    if (!inAir)
     {
       ySpeed -= jumpHeight;
     }
   }
-  void bloodWield(){
+  void bloodWield() {
+  }
+  void damage(int damage)
+  {
+    if (!iFrame)
+    {
+      blood -= damage;
+      damageFrames.start();
+      iFrame = true;
+      flash();
+    }
+  }
+  void flash()
+  {
+    flashing = true;
+    flashFrames.start();
   }
 }
